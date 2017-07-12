ff35bff62f33b66b6c3d5f35c49cd2900a80a44d
ssoso27 <sohi8290@naver.com>
2017-07-12 16:44:32 +0900

    create and setting beerMain, makgeolliMain

---
+++ C2H6O/app/src/main/java/com/example/team1288/c2h6o/Beer_mainActivity.java
@@ -0,0 +1,18 @@
+package com.example.team1288.c2h6o;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ssoso on 2017-07-12.
+ */
+
+public class Beer_mainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.beer_main);
+    }
+}
