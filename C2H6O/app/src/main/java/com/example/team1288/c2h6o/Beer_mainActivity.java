34bc4458ede1e308f2940543e64828d3b9b7eb5a
ssoso27 <sohi8290@naver.com>
2017-07-18 13:22:51 +0900

    create beer page listview

--- C2H6O/app/src/main/java/com/example/team1288/c2h6o/Beer_mainActivity.java
+++ C2H6O/app/src/main/java/com/example/team1288/c2h6o/Beer_mainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 /**
  * Created by ssoso on 2017-07-12.
@@ -17,6 +19,25 @@ public class Beer_mainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.beer_main);
+        ListView listview ;
+        ListViewAdapter adapter;
+
+        // Adapter 생성
+        adapter = new ListViewAdapter() ;
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.beerlist);
+        listview.setAdapter(adapter);
+
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_beer),
+                "Cass", "15%", "1,500", ContextCompat.getDrawable(this, R.drawable.ic_arrow)) ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_beer),
+                "Asahi", "15%", "1,500", ContextCompat.getDrawable(this, R.drawable.ic_arrow)) ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_beer),
+                "Heineken", "15%", "1,500", ContextCompat.getDrawable(this, R.drawable.ic_arrow)) ;
 
     }
 }
