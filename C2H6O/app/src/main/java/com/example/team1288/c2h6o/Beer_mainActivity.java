fa035470085093e37a215e86825d7737b6cc5470
ssoso27 <sohi8290@naver.com>
2017-07-18 13:35:04 +0900

    add listview event

--- C2H6O/app/src/main/java/com/example/team1288/c2h6o/Beer_mainActivity.java
+++ C2H6O/app/src/main/java/com/example/team1288/c2h6o/Beer_mainActivity.java
@@ -1,12 +1,14 @@
 package com.example.team1288.c2h6o;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -39,5 +41,21 @@ public class Beer_mainActivity extends AppCompatActivity {
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_beer),
                 "Heineken", "15%", "1,500", ContextCompat.getDrawable(this, R.drawable.ic_arrow)) ;
 
+        // 위에서 생성한 listview에 클릭 이벤트 핸들러 정의.
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                ListViewItem item = (ListViewItem) parent.getItemAtPosition(position) ;
+
+                String str_name = item.getName() ;
+                String str_degree = item.getDegree() ;
+                String str_price = item.getPrice();
+                Drawable dr_icon = item.getIcon() ;
+                Drawable dr_arrow = item.getArrow();
+
+                // TODO : use item data.
+            }
+        }) ;
     }
 }
