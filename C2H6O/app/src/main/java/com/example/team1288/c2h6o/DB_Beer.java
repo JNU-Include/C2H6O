bb95d29a7fc5b09446304c343b1976f3a0975865
ssoso27 <sohi8290@jejunu.ac.kr>
2017-11-09 19:41:15 +0900

    맥주 추가 1

--- C2H6O/app/src/main/java/com/example/team1288/c2h6o/DB_Beer.java
+++ C2H6O/app/src/main/java/com/example/team1288/c2h6o/DB_Beer.java
@@ -47,18 +47,18 @@ public class DB_Beer extends DB_Alcohol {
 //        insert("스텔라 아르투아", 32, 3200, "스뗄라");
 //        insert("레벤브로이", 33, 3300, "그만하자...");
 
-        insert(R.drawable.beer_cass ,"Cass", 4.5, 0000, "이것은 카스이다...");
-        insert(R.drawable.beer_hite, "Hite", 4.5, 0000, "나는 하이트가 실다....");
-        insert(R.drawable.beer_max, "Max", 4.5, 0000, "맥스 왜먹냐 ㄹㅇ");
-//        insert("Heineken", 18, 1800, "하이네켄...병이..예쁘다...");
+        insert(R.drawable.beer_cass ,"Cass", 4.5, 2400, "이것은 카스이다...");
+        insert(R.drawable.beer_hite, "Hite", 4.5, 2400, "나는 하이트가 실다....");
+        insert(R.drawable.beer_max, "Max", 4.5, 2400, "맥스 왜먹냐 ㄹㅇ");
+        insert(R.drawable.beer_heineken, "Heineken", 5, 3900, "하이네켄...병이..예쁘다...");
 //        insert("Sapporo", 19, 1900, "삿포로 별 존예임 ㄹㅇ");
-//        insert("Hoegaarden", 20, 2000, "로제 마셔보고 싶다");
+        insert(R.drawable.beer_hoegaarden, "Hoegaarden", 20, 2000, "국내 최초로 소개된 화이트 맥주. \n오렌지 껍질이 들어가 향긋하다.");
 //        insert("Hoegaarden Rosse", 21, 2100, "마셔보고 싶어서 넣어봤다");
 //        insert("Asahi", 22, 2200, "먹을만한데 좀 씀");
 //        insert("Asahi Dry Black", 23, 2300, "아사히랑 다른건가?");
 //        insert("San Miguel", 24, 2400, "비싸다 안먹어봤다");
 //        insert("Tsingtao", 25, 2500, "양꼬치엔 칭따오!");
-//        insert("Kozel Dark", 26, 2600, "흑맥주래;");
+        insert(R.drawable.beer_kozel, "Kozel Dark", 26, 2600, "쌉싸래한 동시에 달콤한 맛이 특징인 흑맥주. \n최고의 맥주로 여덟 차례 선정되었다.");
 //        insert("Weidann Schwarzbier", 27, 2700, "흑맥주 밀맥주 구분란 넣을까...");
 //        insert("Kronenbourg 1664", 28, 2800, "이름 존나김");
 //        insert("Pilsner Urguell", 29, 2900, "그만 치고 싶다");
